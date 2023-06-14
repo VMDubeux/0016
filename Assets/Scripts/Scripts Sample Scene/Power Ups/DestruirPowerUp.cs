6534fa7ae5f0201663607930f022326b48601a6d
Victor Moura Dubeux <adv.victordubeux@gmail.com>
2023-06-14 15:56:57 -0300

    Commit 46
    Ajuste dos PowerUps, acrescentando limitador de borda (quando passam da borda esquerda eles são destruídos), bem como implementação do sistema de partículas e seus respectivos ajustes de rotação. Funcionando plenamente.

--- Assets/Scripts/Scripts Sample Scene/Power Ups/DestruirPowerUp.cs
+++ Assets/Scripts/Scripts Sample Scene/Power Ups/DestruirPowerUp.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestruirPowerUp : MonoBehaviour
 {
     public float tempoDeVida;
+    private float _leftBound = -370f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,6 @@ public class DestruirPowerUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < _leftBound && gameObject.CompareTag("PowerUp")) Destroy(gameObject);
     }
 }
